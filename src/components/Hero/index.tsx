b8d4969ea9d221f09191093d60e40088bd6f79ae
Carlos Oliveira <kaduchaos18@gmail.com>
2025-09-14 20:24:42 -0300

    styles: adapting hero styles

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,13 @@
-import styles from './Hero.module.css'
+import { Banner, ContainerHero, DescriptionHero } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
+  <Banner>
+    <ContainerHero>
+      <DescriptionHero>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+      </DescriptionHero>
+    </ContainerHero>
+  </Banner>
 )
 
 export default Hero
